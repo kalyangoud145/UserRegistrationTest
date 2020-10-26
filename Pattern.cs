996214c36627a7823b3a36aae300180d71ed8649
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-26 16:41:51 +0530

    [Kalyan] added password pattern with one upper case

--- Pattern.cs
+++ Pattern.cs
@@ -8,11 +8,11 @@ namespace PincodePattern
     public class Pattern
     {
         //The regex pattern for password
-        public static string REGEX_PASSWORD = ".{8,}";
+        public static string REGEX_PASSWORD = "^(?=.*[A-Z]).{8,}$";
         /// <summary>
-        /// Validates the password such that with minimum of 8 charecters
-        /// By taking password as argument.
-        /// And returns true or false
+        /// Validates the password such that with atleast one uppercase charecter
+        /// And minimum of 8 charecters, By taking password as argument.
+        /// Returns true or false
         /// </summary>
         /// <param name="password">The password.</param>
         /// <returns></returns>
