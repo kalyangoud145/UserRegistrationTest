4468028b50f4e7b22cfdceafd12f55d89626bc12
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-26 17:45:02 +0530

    [Kalyan] added pattern for exactly one special charecter

--- Pattern.cs
+++ Pattern.cs
@@ -8,10 +8,11 @@ namespace PincodePattern
     public class Pattern
     {
         //The regex pattern for password
-        public static string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+        public static string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])(?=[\\w]*[\\W][\\w]*$)(?=.{8,}$).*$";
         /// <summary>
         /// Validates the password such that with atleast one uppercase charecter
-        /// And atleast one numeric, length of minimum  8 charecters
+        /// And atleast one numeric,exact one special charecter 
+        /// and length of minimum  8 charecters
         /// By taking password as argument and Returns true or false
         /// </summary>
         /// <param name="password">The password.</param>
