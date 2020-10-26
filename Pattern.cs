c37f19113d20ac018b63fedb69dbe3a97acae156
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-26 13:33:37 +0530

    [Kalyan] added method for validating the first name

--- Pattern.cs
+++ Pattern.cs
@@ -5,8 +5,20 @@ using System.Text.RegularExpressions;
 
 namespace PincodePattern
 {
-     public class Pattern
+    public class Pattern
     {
-        
+        //The regex pattern for firstname
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[a-zA-Z]{2,}";
+        /// <summary>
+        /// Validates the first name by taking firstname as argument.
+        /// And returns true or false
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns></returns>
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,11 +2,21 @@
 
 namespace PincodePattern
 {
+    /// <summary>
+    /// Main Class for calling the respective methods
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Problem");
+            //Pattern method object is created
+            Pattern pattern = new Pattern();
+            //Gets user input and store in firstname variable
+            Console.WriteLine("Enter First Name");
+            string firstName = Console.ReadLine();
+            //Here it calls ValidateFirstName method for validating the firstname
+            //parameter for the method is firstName
+            Console.WriteLine(pattern.ValidateFirstName(firstName));
         }
     }
 }
